ed6fcc8dbc7412c1e229e3fc1c0cba6659a832d3
Mehdi Rachico <mehdirachico905@gmail.com>
2018-01-06 16:49:44 +0100

    Fisrt pages commit

--- scriptOfWTTFY.js
+++ scriptOfWTTFY.js
@@ -18,6 +18,70 @@ $(function() {
 	 $('.name').hover(function() {
 		$(this).toggleClass('red');
 	});
+
+	 $('.a1').hover(function(){
+	 	$(this).toggleClass('red');
+	 });
+
+
+	 $('.a2').hover(function(){
+	 	$(this).toggleClass('red');
+	 });
+	
+
+	 $('.a3').hover(function(){
+	 	$(this).toggleClass('red');
+	 });
+	
+
+	 $('.a4').hover(function(){
+	 	$(this).toggleClass('red');
+	 });
+	
+
+	 $('.a5').hover(function(){
+	 	$(this).toggleClass('red');
+	 });
+	
+
+	 $('.a6').hover(function(){
+	 	$(this).toggleClass('red');
+	 });
+	
+
+	 $('.a7').hover(function(){
+	 	$(this).toggleClass('red');
+	 });
+
+	 $('.a8').hover(function(){
+	 	$(this).toggleClass('red');
+	 });
+
+
+	 $('.a9').hover(function(){
+	 	$(this).toggleClass('red');
+	 });
+
+
+	 $('.a10').hover(function(){
+	 	$(this).toggleClass('red');
+	 });
+
+
+	 $('.a11').hover(function(){
+	 	$(this).toggleClass('red');
+	 });
+
+
+	 $('.a12').hover(function(){
+	 	$(this).toggleClass('red');
+	 });
+	
+	
+	
+	
+	
+	
 	
 	
 
